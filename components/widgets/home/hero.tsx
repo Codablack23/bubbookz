bc7496faa9fe001c9c8931091250092b956aab62
Codablack <goodluckedih@yahoo.com>
2022-11-24 22:24:28 +0100

    added slider

--- components/widgets/home/hero.tsx
+++ components/widgets/home/hero.tsx
@@ -1,10 +1,13 @@
+import { Carousel } from 'antd';
+
 export default function Hero(){
   const goToDashboard = ()=>{
     window.location.replace("/dashboard")
   }  
   return(
-        <div className="Hero">
-         <div className="hero-content">
+    <Carousel autoplay dots={{className:"thumb"}}>
+        <div className="Hero"> 
+          <div className="hero-content">
            <h1 className="hero-heading">
            You’ll Find the worlds best <span className="text-theme">book</span> series on
            <span className="text-theme"> Bubbooks.</span>
@@ -20,6 +23,28 @@ export default function Hero(){
            </span>
            </button>
          </div>
+         </div>
+         <div className='Hero bg-2'>
+         <div className="hero-content">
+           <h1 className="hero-heading">
+           Adventure, Action, Comedy  <span className="text-theme">book</span> series on
+           <span className="text-theme"> Bubbooks.</span>
+           </h1>
+           <p className="hero-text">
+           Get Books for fun 
+           </p>
+           <button className="hero-btn" onClick = {goToDashboard}>
+             <span>Shop Now</span>
+           <span>
+             <span></span>
+             <i className="bi bi-arrow-right"></i>
+           </span>
+           </button>
+         </div>
+         
         </div>
+      </Carousel>
     )
 }
+
+
