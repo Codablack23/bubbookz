056be551162f33514530b37ee642d93f66f88432
Codablack <goodluckedih@yahoo.com>
2022-11-24 13:14:28 +0100

    fixed some bugs

---
+++ pages/dashboard/communities/[cid].tsx
@@ -0,0 +1,9 @@
+import DashboardLayout from "~/components/layout/dashboard/DashboardLayout";
+
+export default function DashboardCommunityPage(){
+    return (
+        <DashboardLayout activePage={"community"}>
+          <h1>Hello World</h1>
+        </DashboardLayout>
+    )
+}
